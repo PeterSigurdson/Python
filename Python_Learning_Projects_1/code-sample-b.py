6e5120263b5ea3fd06c7e7a6d39ed27ae95a30c1
Peter Sigurdson <peter@petersigurdson.net>
2018-08-03 10:55:42 -0400

    Some Code Updates

---
+++ Python_Learning_Projects_1/code-sample-b.py
@@ -0,0 +1,9 @@
+class lion:
+    def __init__(self,name,age):
+        self.name:name
+        self.age:age
+    def growl(self):
+        print("grawo")
+        
+ob1 = lion("name", 17)
+ob1.growl()
